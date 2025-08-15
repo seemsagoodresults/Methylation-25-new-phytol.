b4a1e8a76af83831ff99776d1b7d3bfd0503ef75
Yi-lei-Wang <102602898+seemsagoodresults@users.noreply.github.com>
2025-08-15 11:36:20 +0800

    Delete 0TEannotation.sh

--- 0TEannotation.sh
+++
@@ -1,3 +0,0 @@
-BuildDatabase -name nm -engine ncbi /home/wangyilei2020/All_genome_data/lemon_genome/Climon_v1_primary-scaffolds.fa
-nohup RepeatModeler -database nm -engine ncbi -pa 20 &> nm.out&
-nohup RepeatMasker -lib ../02repeatModeler/nm-families.fa -e ncbi -pa 10 -dir . /home/wangyilei2020/All_genome_data/lemon_genome/Climon_v1_primary-scaffolds.fa&
